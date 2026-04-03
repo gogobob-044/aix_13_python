9de3f54066e957e63cae3436ea1975d7d577c0c5
gogobob-044 <yena0875@gmail.com>
2026-04-03 16:05:55 +0900

    뉴스목록 크롤링

---
+++ 2026.04.03/07-2-test-news.py
@@ -0,0 +1,16 @@
+from urllib import request
+from bs4 import BeautifulSoup
+
+
+def get_news():
+    url = "https://news.naver.com/section/105"
+    target = request.urlopen(url)
+    soup = BeautifulSoup(target, "html.parser")
+
+    tag = "li.rl_item.rl_txt" #부모 자식을 보가며 클래스 찾기
+    news = soup.select(tag)
+    for t in news:
+        title = t.get_text()
+        print(title)
+
+get_news()
